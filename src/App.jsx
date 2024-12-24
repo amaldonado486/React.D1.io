70dec29b4757dc253b38fc1a6488732a05fc5bf1
amaldonado486 <amaldonado486@gmail.com>
2024-12-24 18:12:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Home />
+    <Footer />
+  </>
+);
+
+
+export default App
